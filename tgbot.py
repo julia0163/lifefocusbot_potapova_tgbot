9e68a8d8576ca5c5305c4c256ab8f38eceb1574d
julia0163 <potapova.julia.0163@gmail.com>
2025-06-29 10:55:48 +0400

    Update tgbot.py

--- tgbot.py
+++ tgbot.py
@@ -1,29 +1,22 @@
 import os
 from telegram import Update
-from telegram.ext import (
-    ApplicationBuilder,
-    ContextTypes,
-    CommandHandler,
-)
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-# Замените своим токеном
 BOT_TOKEN = os.getenv("TOKEN")
-WEBHOOK_HOST = os.getenv("RENDER_EXTERNAL_HOSTNAME")  # от Render
+WEBHOOK_HOST = os.getenv("RENDER_EXTERNAL_HOSTNAME")
 WEBHOOK_PATH = f"/{BOT_TOKEN}"
 WEBHOOK_URL = f"https://{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет! Бот работает 🎉")
+    await update.message.reply_text("Бот успешно работает! 🚀")
 
 
 def main():
-    application = ApplicationBuilder().token(BOT_TOKEN).build()
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
 
-    application.add_handler(CommandHandler("start", start))
-
-    # Установка webhook-а
-    application.run_webhook(
+    app.run_webhook(
         listen="0.0.0.0",
         port=int(os.environ.get("PORT", 8443)),
         webhook_url=WEBHOOK_URL,
