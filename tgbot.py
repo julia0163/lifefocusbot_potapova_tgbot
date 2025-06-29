69bd1004f61d4e232afa41c58d7e20572dd3473f
julia0163 <potapova.julia.0163@gmail.com>
2025-06-29 11:11:17 +0400

    Update tgbot.py

--- tgbot.py
+++ tgbot.py
@@ -1,6 +1,7 @@
 import os
+import asyncio
 from telegram import Update
-from telegram.ext import Application, ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 BOT_TOKEN = os.getenv("TOKEN")
 RENDER_EXTERNAL_HOSTNAME = os.getenv("RENDER_EXTERNAL_HOSTNAME")
@@ -9,16 +10,16 @@ PORT = int(os.environ.get("PORT", 8443))
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Бот успешно работает! 🚀")
 
-def main():
-    application: Application = ApplicationBuilder().token(BOT_TOKEN).build()
+async def main():
+    application = ApplicationBuilder().token(BOT_TOKEN).build()
     application.add_handler(CommandHandler("start", start))
 
-    # Установка вебхука
-    application.run_webhook(
+    # Запускаем вебхук
+    await application.run_webhook(
         listen="0.0.0.0",
         port=PORT,
         webhook_url=f"https://{RENDER_EXTERNAL_HOSTNAME}/{BOT_TOKEN}",
     )
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
