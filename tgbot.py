f9d7e3e9fde97c7799a6bf4a44dbf9e51464feea
julia0163 <potapova.julia.0163@gmail.com>
2025-06-29 11:02:46 +0400

    Update tgbot.py

--- tgbot.py
+++ tgbot.py
@@ -1,27 +1,24 @@
 import os
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import Application, ApplicationBuilder, CommandHandler, ContextTypes
 
 BOT_TOKEN = os.getenv("TOKEN")
-WEBHOOK_HOST = os.getenv("RENDER_EXTERNAL_HOSTNAME")
-WEBHOOK_PATH = f"/{BOT_TOKEN}"
-WEBHOOK_URL = f"https://{WEBHOOK_HOST}{WEBHOOK_PATH}"
-
+RENDER_EXTERNAL_HOSTNAME = os.getenv("RENDER_EXTERNAL_HOSTNAME")
+PORT = int(os.environ.get("PORT", 8443))
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Бот успешно работает! 🚀")
 
-
 def main():
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
+    application: Application = ApplicationBuilder().token(BOT_TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
 
-    app.run_webhook(
+    # Установка вебхука
+    application.run_webhook(
         listen="0.0.0.0",
-        port=int(os.environ.get("PORT", 8443)),
-        webhook_url=WEBHOOK_URL,
+        port=PORT,
+        webhook_url=f"https://{RENDER_EXTERNAL_HOSTNAME}/{BOT_TOKEN}",
     )
-
 
 if __name__ == "__main__":
     main()
